66cc5ebe492ffb4e8d2ecd6226467e9cc21e1a4b
Michael Dolan <michaeldolan03@gmail.com>
2025-07-21 13:54:02 -0400

    Setup fixes
    Had to make a few name changes in settings.py for Django/project to recognize stuff and for it to actually launch the development server when running the manage.py file

--- django/DjangoApi/DjangoApi/settings.py
+++ django/DjangoApi/DjangoApi/settings.py
@@ -37,9 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest-framework',
+    'rest_framework',
     'corsheaders',
-    'LiftTracker.apps.LiftTrackerConfig',
+    'LiftTracker',
 ]
 
 # CORS settings
