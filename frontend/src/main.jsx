bcf3ec0eb4ddc0a0af41afd0e3d1638597dedf03
Michael Dolan <michaeldolan03@gmail.com>
2025-07-25 00:59:11 -0400

    First part of Landing Page made using first Figma design
    Contains the 3 main stats and navigations, buttons link to separate pages, Enter lift button for database/Django communication still works on this page

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+// Removed root from index.css I think so not sure how this is working
 createRoot(document.getElementById('root')).render(
   // <StrictMode>
     <App />
