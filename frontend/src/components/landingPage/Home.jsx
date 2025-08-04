4743ac02847299608e90e83368467a62ef91920b
Michael Dolan <michaeldolan03@gmail.com>
2025-08-04 13:09:09 -0400

    Initial new landing page made
    Note: Isn't responsive/does not do well with different screen resolutions, Sign Up page and Log In page do not exist yet

--- frontend/src/components/landingPage/Home.jsx
+++ frontend/src/components/landingPage/Home.jsx
@@ -4,7 +4,13 @@ function Home() {
     return (
         <div className="image">
             <div className="SignUp-Login">
-                
+                <div className="Msg">
+                    <p className="Ad">Get started tracking lifts and connecting with friends today</p>
+                </div>
+                <div className="buttons">
+                    <button className="account-buttons">Sign Up</button>
+                    <button className="account-buttons">Log In</button>
+                </div>
             </div>
         </div>
     );
